3e7bb22f0414bf7e8b5a6f21f27a9e34a3cf8435
angelajasso <angelajasso@hotmail.com>
2025-02-20 21:20:18 -0600

    add buttons

--- portfolio_data/portfolio_data.py
+++ portfolio_data/portfolio_data.py
@@ -1,39 +1,18 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
 
-from rxconfig import config
-
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+def index():
+    return rx.hstack(
+        rx.button(
+            "Decrement",
+            color_scheme="ruby",
         ),
-        rx.logo(),
+        rx.heading(font_size="2em"),
+        rx.button(
+            "Increment",
+            color_scheme="grass",
+        ),
+        spacing="4",
     )
-
 
 app = rx.App()
 app.add_page(index)
